283ebda8598698dea423742d568d4b100fedd507
caoliduo <894871274@qq.com>
2020-09-16 23:01:39 +0800

    练习1

---
+++ Card.java
@@ -0,0 +1,23 @@
+package com.caoliduo.card;
+
+public class Card {
+    public String suit;
+    public int rank;
+    public Card(String suit,int rank){
+        this.suit=suit;
+        this.rank=rank;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s %d]",suit,rank);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this==obj) return true;
+        if(obj==null||!(obj instanceof Card))return  false;
+        Card card=(Card) obj;
+        return  rank==card.rank && suit.equals(card.suit);
+    }
+}
